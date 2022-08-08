f1100214c5df55899a9b345c266fb9dedb8c660d
Daniel Lindsley <daniel@toastdriven.com>
2022-08-08 18:51:10 -0500

    Added term position support. (#7)
    * Initial rework to include term positions.
    
    * Docs & comments.
    
    * Whoops. Terminology.
    
    * A couple more tests, especially of new API bits.

--- test/minisearch.test.js
+++ test/minisearch.test.js
@@ -1,10 +1,113 @@
 import assert from "assert";
 
 import {
-  BasicEnglishPreprocessor,
+  VERSION,
+  TermPosition,
+  BasicPreprocessor,
   NGramTokenizer,
   MiniSearch,
 } from "../src/index.js";
+
+describe("BasicEnglishPreprocessor", function () {
+  describe("default processing", function () {
+    it("returns the correct terms", function () {
+      const pp = new BasicPreprocessor();
+      const doc = "The dog is hot.";
+
+      const terms = pp.process(doc);
+      assert.equal(terms.length, 4);
+      assert.equal(terms[0].term, "the");
+      assert.equal(terms[0].position, 0);
+      assert.equal(terms[1].term, "dog");
+      assert.equal(terms[1].position, 4);
+      assert.equal(terms[2].term, "is");
+      assert.equal(terms[2].position, 8);
+      assert.equal(terms[3].term, "hot");
+      assert.equal(terms[3].position, 11);
+    });
+
+    it("returns the correct terms with surrounding whitespace", function () {
+      const pp = new BasicPreprocessor();
+      const doc = " The dog is hot.\n ";
+
+      const terms = pp.process(doc);
+      assert.equal(terms.length, 4);
+      assert.equal(terms[0].term, "the");
+      assert.equal(terms[0].position, 1);
+      assert.equal(terms[1].term, "dog");
+      assert.equal(terms[1].position, 5);
+      assert.equal(terms[2].term, "is");
+      assert.equal(terms[2].position, 9);
+      assert.equal(terms[3].term, "hot");
+      assert.equal(terms[3].position, 12);
+    });
+  });
+
+  describe("overridden processing", function () {
+    it("correctly handles different splits", function () {
+      const pp = new BasicPreprocessor(/[\n]/);
+      const doc = "The dog\nis hot.\n\n";
+
+      const terms = pp.process(doc);
+      assert.equal(terms.length, 2);
+      assert.equal(terms[0].term, "the dog");
+      assert.equal(terms[0].position, 0);
+      assert.equal(terms[1].term, "is hot");
+      assert.equal(terms[1].position, 8);
+    });
+
+    it("avoids stop words", function () {
+      const pp = new BasicPreprocessor(null, ["the", "a", "an", "is"]);
+      const doc = "The dog is a 'hot dog'.";
+
+      const terms = pp.process(doc);
+      assert.equal(terms.length, 3);
+      assert.equal(terms[0].term, "dog");
+      assert.equal(terms[0].position, 4);
+      assert.equal(terms[1].term, "hot");
+      assert.equal(terms[1].position, 13);
+      assert.equal(terms[2].term, "dog");
+      assert.equal(terms[2].position, 18);
+    });
+  });
+});
+
+describe("NGramTokenizer", function () {
+  describe("default processing", function () {
+    it("returns the correct n-grams", function () {
+      const ng = new NGramTokenizer();
+      const word = "doggone";
+
+      const tokens = ng.tokenize(word);
+      assert.equal(tokens.length, 5);
+      assert.deepEqual(tokens, [
+        "dog",
+        "ogg",
+        "ggo",
+        "gon",
+        "one",
+      ]);
+    });
+  });
+
+  describe("overridden processing", function () {
+    it("returns shorter n-grams", function () {
+      const ng = new NGramTokenizer(2);
+      const word = "doggone";
+
+      const tokens = ng.tokenize(word);
+      assert.equal(tokens.length, 6);
+      assert.deepEqual(tokens, [
+        "do",
+        "og",
+        "gg",
+        "go",
+        "on",
+        "ne",
+      ]);
+    });
+  });
+});
 
 describe("MiniSearch", function () {
   describe("constructor", function () {
@@ -14,6 +117,7 @@ describe("MiniSearch", function () {
       assert.deepEqual(engine.index, {
         "documentIds": {},
         "terms": {},
+        "version": VERSION,
       });
     });
   });
